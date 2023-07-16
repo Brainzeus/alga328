0323b58e0ae9b445f1cccd631caf79ea2c10c30b
Brainzeus <89934607+Brainzeus@users.noreply.github.com>
2023-07-16 12:31:07 +0300

    Add files via upload

---
+++ mon.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+#This is an example you can edit and use
+#There are numerous parameters you can set, please check Help and Examples folder
+
+SRBMiner-MULTI.exe --algorithm ghostrider --pool stratum-eu.rplant.xyz:17096 --wallet AFtT4Mg32KnmtkohFa3UBMVmpiPkJy8rH2 --password brainers --cpu-threads 0 --ennable-gpu
